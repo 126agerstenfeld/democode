2921722d47bd0e57e46d5b06539e0799d1ea68ff
Alex <113116242+126agerstenfeld@users.noreply.github.com>
2026-01-03 13:23:38 -0500

    small fix

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import socket
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(("0.0.0", 12345))
+server.bind(("0.0.0.0", 12345))
 server.listen(1)
 
 print("Waiting for connection")
